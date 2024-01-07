460a02c15779cb9ca969da25b4c25b0244ba1d51
Carlos Lezama <carloselezamaj@gmail.com>
2024-01-06 18:15:07 -0600

    add typst lsp

--- nvim/custom/configs/conform.lua
+++ nvim/custom/configs/conform.lua
@@ -1,25 +1,26 @@
 local options = {
-  lsp_fallback = true,
+	lsp_fallback = true,
 
-  formatters_by_ft = {
-    css = { "prettier" },
-    graphql = { "prettier" },
-    html = { "prettier" },
-    javascript = { "prettier" },
-    json = { "prettier" },
-    latex = { "latexindent" },
-    lua = { "stylua" },
-    markdown = { "prettier" },
-    python = { "ruff_format" },
-    rust = { "rustfmt" },
-    scss = { "prettier" },
-    sh = { "shfmt" },
-    sql = { "sqlfluff" },
-    terraform = { "terraform_fmt" },
-    toml = { "taplo" },
-    xml = { "xmlformat" },
-    yaml = { "prettier" },
-  }
+	formatters_by_ft = {
+		css = { "prettier" },
+		graphql = { "prettier" },
+		html = { "prettier" },
+		javascript = { "prettier" },
+		json = { "prettier" },
+		latex = { "latexindent" },
+		lua = { "stylua" },
+		markdown = { "prettier" },
+		python = { "ruff_format" },
+		rust = { "rustfmt" },
+		scss = { "prettier" },
+		sh = { "shfmt" },
+		sql = { "sqlfluff" },
+		terraform = { "terraform_fmt" },
+		toml = { "taplo" },
+		typst = { "typstfmt" },
+		xml = { "xmlformat" },
+		yaml = { "prettier" },
+	},
 }
 
 require("conform").setup(options)
